fd6f78009889f63e3bab38dea4d7eab5df0c5cd9
Dennis Abad <dennis.abad6@gmail.com>
2023-02-28 11:42:48 +1100

    Added OperationsController to consolidate all operations into one controller Added unit tests for subtract, multiply, divide operations Remove AddController

---
+++ CalculatorAPI/Controllers/OperationsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationsController : ControllerBase
+    {
+        [Route("Add")]
+        [HttpGet]
+        public decimal GetByAddition([FromQuery] decimal number1, [FromQuery] decimal number2)
+        {
+            return number1 + number2;
+        }
+
+        [Route("Subtract")]
+        [HttpGet]
+        public decimal GetBySubtraction([FromQuery] decimal number1, [FromQuery] decimal number2)
+        {
+            return number1 - number2;
+        }
+
+        [Route("Multiply")]
+        [HttpGet]
+        public decimal GetByMultiplication([FromQuery] decimal number1, [FromQuery] decimal number2)
+        {
+            return number1 * number2;
+        }
+
+        [Route("Divide")]
+        [HttpGet]
+        public decimal GetByDivision([FromQuery] decimal number1, [FromQuery] decimal number2)
+        {
+            return number1 / number2;
+        }
+    }
+}
--- CalculatorAPITest/OperationsTest.cs
+++ CalculatorAPITest/OperationsTest.cs
@@ -4,18 +4,59 @@ namespace CalculatorAPITest
 {
     public class OperationsTest
     {
+        private readonly OperationsController _controller;
+
+        public OperationsTest()
+        {
+            _controller = new OperationsController();
+        }
+
         [Fact]
         public void Get_WhenCalled_AddsValues()
         {
-            var addController = new AddController();
+            Assert.Equal(10, _controller.GetByAddition(2, 8));
+            Assert.Equal(12, _controller.GetByAddition(6, 6));
+            Assert.Equal(88, _controller.GetByAddition(24, 64));
 
-            Assert.Equal(10, addController.Get(2, 8));
-            Assert.Equal(12, addController.Get(6, 6));
-            Assert.Equal(88, addController.Get(24, 64));
+            Assert.NotEqual(1, _controller.GetByAddition(4, 3));
+            Assert.NotEqual(25, _controller.GetByAddition(5, 5));
+            Assert.NotEqual(13, _controller.GetByAddition(39, 3));
+        }
 
-            Assert.NotEqual(1, addController.Get(4, 3));
-            Assert.NotEqual(25, addController.Get(5, 5));
-            Assert.NotEqual(13, addController.Get(39, 3));
+        [Fact]
+        public void Get_WhenCalled_SubtractsValues()
+        {
+            Assert.Equal(1, _controller.GetBySubtraction(4, 3));
+            Assert.Equal(0, _controller.GetBySubtraction(6, 6));
+            Assert.Equal(40, _controller.GetBySubtraction(64, 24));
+
+            Assert.NotEqual(10, _controller.GetBySubtraction(2, 8));
+            Assert.NotEqual(25, _controller.GetBySubtraction(5, 5));
+            Assert.NotEqual(13, _controller.GetBySubtraction(39, 3));
+        }
+
+        [Fact]
+        public void Get_WhenCalled_MultipliesValues()
+        {
+            Assert.Equal(12, _controller.GetByMultiplication(4, 3));
+            Assert.Equal(36, _controller.GetByMultiplication(6, 6));
+            Assert.Equal(1536, _controller.GetByMultiplication(64, 24));
+
+            Assert.NotEqual(10, _controller.GetByMultiplication(2, 8));
+            Assert.NotEqual(0, _controller.GetByMultiplication(5, 5));
+            Assert.NotEqual(13, _controller.GetByMultiplication(39, 3));
+        }
+
+        [Fact]
+        public void Get_WhenCalled_DividesValues()
+        {
+            Assert.Equal(4, _controller.GetByDivision(12, 3));
+            Assert.Equal(1, _controller.GetByDivision(6, 6));
+            Assert.Equal(3, _controller.GetByDivision(72, 24));
+
+            Assert.NotEqual(10, _controller.GetByDivision(2, 8));
+            Assert.NotEqual(25, _controller.GetByDivision(5, 5));
+            Assert.NotEqual(10, _controller.GetByDivision(13, 3));
         }
     }
 }
