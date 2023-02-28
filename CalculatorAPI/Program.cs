60a41f9f7362f33aa0a12a3d8011a8e47d62fd36
Dennis Abad <dennis.abad6@gmail.com>
2023-02-28 15:32:28 +1100

    Added CORS policy to enable cors

--- CalculatorAPI/Program.cs
+++ CalculatorAPI/Program.cs
@@ -7,6 +7,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigins",
+        policy => policy.WithOrigins("https://localhost:7294")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod());
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +24,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowSpecificOrigins");
 
 app.UseHttpsRedirection();
 
