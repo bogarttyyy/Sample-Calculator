035284171b4bb420d0181f34edd5a49ad43ce2b3
Dennis Abad <dennis.abad6@gmail.com>
2023-02-28 11:24:38 +1100

    Added Add operation + test

---
+++ CalculatorAPI/Controllers/AddController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CalculatorAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddController : ControllerBase
+    {
+        // GET api/<AddController>/5/10
+        [HttpGet]
+        public decimal Get([FromQuery]decimal number1, [FromQuery]decimal number2)
+        {
+            return number1 + number2;
+        }
+    }
+}
---
+++ CalculatorAPITest/OperationsTest.cs
@@ -0,0 +1,21 @@
+﻿using CalculatorAPI.Controllers;
+
+namespace CalculatorAPITest
+{
+    public class OperationsTest
+    {
+        [Fact]
+        public void Get_WhenCalled_AddsValues()
+        {
+            var addController = new AddController();
+
+            Assert.Equal(10, addController.Get(2, 8));
+            Assert.Equal(12, addController.Get(6, 6));
+            Assert.Equal(88, addController.Get(24, 64));
+
+            Assert.NotEqual(1, addController.Get(4, 3));
+            Assert.NotEqual(25, addController.Get(5, 5));
+            Assert.NotEqual(13, addController.Get(39, 3));
+        }
+    }
+}
